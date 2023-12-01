555a8e497e2d263f291066ce25e32f400d45eacb
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-12-01 23:01:27 +0700

    trang chi tiết bài hát

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -32,3 +32,33 @@ export const list = async (req: Request, res: Response) => {
     songs: songs
   });
 };
+
+// [GET] /songs/detail/:slugSong
+export const detail = async (req: Request, res: Response) => {
+  const slugSong: string = req.params.slugSong;
+
+  const song = await Song.findOne({
+    slug: slugSong,
+    status: "active",
+    deleted: false
+  });
+
+  const singer = await Singer.findOne({
+    _id: song.singerId,
+    status: "active",
+    deleted: false
+  }).select("fullName");
+
+  const topic = await Topic.findOne({
+    _id: song.topicId,
+    status: "active",
+    deleted: false
+  }).select("title");
+
+  res.render("client/pages/songs/detail", {
+    pageTitle: "Chi tiết bài hát",
+    song,
+    singer,
+    topic
+  })
+}
